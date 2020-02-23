ca6a385f5bbc2f7d629e76c4b93dd880f1a40d39
Javier Gutierrez <javier.guse@icloud.com>
2020-02-23 10:38:52 -0300

    while and loop

--- lesson3_control_flow/src/main.rs
+++ lesson3_control_flow/src/main.rs
@@ -1,5 +1,7 @@
 mod if_statement;
+mod while_and_loop;
 
 fn main() {
-    if_statement::if_statement();
+    // if_statement::if_statement();
+    while_and_loop::while_and_loop();
 }
---
+++ lesson3_control_flow/src/while_and_loop.rs
@@ -0,0 +1,20 @@
+pub fn while_and_loop() {
+    let mut x = 1;
+
+    while x < 1000 {
+        x *= 2;
+
+        if x == 64 { continue; }
+
+        println!("x = {}", x);
+    }
+
+    let mut y = 1;
+    loop // while true, exiting has to be explicit
+    {
+        y *= 2;
+        println!("y = {}", y);
+
+        if y == 1 << 10 { break; }
+    }
+}
