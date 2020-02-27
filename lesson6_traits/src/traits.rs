063a29fdaa97d31c1413d6ee95fb3d7ab4ef60ca
Javier Gutierrez <javier.guse@icloud.com>
2020-02-26 22:57:53 -0300

    start lesson6 with traits

---
+++ lesson6_traits/src/traits.rs
@@ -0,0 +1,76 @@
+trait Animal {
+    // This is a static function (like a constructor)
+    // notice that it doesnt receive self
+    fn create(name: &'static str) -> Self;
+
+    fn name(&self) -> &'static str;
+
+    // talk is default implementation that can be override
+    fn talk(&self) {
+        println!("{} cannot talk", self.name());
+    }
+}
+
+struct Human {
+    name: &'static str
+}
+
+struct Cat {
+    name: &'static str
+}
+
+impl Animal for Human {
+    fn create(name: &'static str) -> Human {
+        Human { name: name }
+    }
+
+    fn name(&self) -> &'static str {
+        self.name
+    }
+
+    // override the default implementation so that humans can talk
+    fn talk(&self) {
+        println!("{} says hello", self.name());
+    }
+}
+
+impl Animal for Cat {
+    fn create(name: &'static str) -> Cat {
+        Cat { name: name }
+    }
+
+    fn name(&self) -> &'static str {
+        self.name
+    }
+
+    // override the default implementation so that humans can talk
+    fn talk(&self) {
+        println!("{} says meow", self.name());
+    }
+}
+
+// Define trait for an existing Type
+trait Summable<T> {
+    fn sum(&self) -> T;
+}
+
+impl Summable<i32> for Vec<i32> {
+    fn sum(&self) -> i32 {
+        let mut result: i32 = 0;
+        for x in self { result+= *x; }
+        return result;
+    }
+}
+
+pub fn enter() {
+    // let h = Human { name: "John" };
+    // let h = Human::create("John");
+    let h: Human = Animal::create("John");
+    h.talk();
+
+    let c = Cat { name: "Misty" };
+    c.talk();
+
+    let a = vec![1,2,3];
+    println!("sum = {}", a.sum())
+}
