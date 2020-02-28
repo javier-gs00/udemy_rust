a94f86d7b1b71f1ac7b55521f0e4408982876121
Javier Gutierrez <javier.guse@icloud.com>
2020-02-28 20:05:26 -0300

    why dynamic dispatch

--- lesson6_traits/src/main.rs
+++ lesson6_traits/src/main.rs
@@ -7,6 +7,7 @@ mod drop;
 mod operator_overloading;
 mod static_dispatch;
 mod dynamic_dispatch;
+mod why_dynamic_dispatch;
 
 fn main() {
     // traits::enter();
@@ -15,5 +16,6 @@ fn main() {
     // drop::enter();
     // operator_overloading::enter();
     // static_dispatch::enter();
-    dynamic_dispatch::enter()
+    // dynamic_dispatch::enter()
+    why_dynamic_dispatch::enter()
 }
